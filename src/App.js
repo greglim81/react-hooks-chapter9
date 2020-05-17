eac76f0639e54091fd95fd9fe62e62bb1c801fb8
Greg Lim <limjunqu@gmail.com>
2020-05-17 18:14:04 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React, { useReducer} from 'react';
 import ToDoList from './ToDoList'
-import { v4 as uuidv4 } from 'uuid';
 
 const todosInitialState = { 
-  todos:[{ id:1, text: "finishing writing hooks chapter"},
-    { id:2, text: "play with kids"},
-    { id:3, text: "read bible"}
-  ]
+  todos:[]
 };
 
 export const TodosContext = React.createContext()
@@ -23,9 +19,10 @@ function App (){
 
 function todosReducer(state, action){ 
   switch(action.type){     
-    case 'add':
-      const newToDo = {id: uuidv4(), text: action.payload}
-      const addedToDos = [...state.todos,newToDo]
+    case 'get':     
+      return {...state,todos:action.payload}    
+    case 'add':      
+      const addedToDos = [...state.todos,action.payload]
       return {...state,todos:addedToDos}
     case 'delete':
       const filteredTodoState = state.todos.filter( todo => todo.id !== action.payload.id)
